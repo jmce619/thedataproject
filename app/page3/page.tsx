e8ba0b127400faa15c53ddb83d13d8b95f30a209
Jamie <jmcelhiney619@gmail.com>
2025-05-18 20:21:14 -0400

    try page 3 alternate

---
+++ app/page3/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useState, useEffect, ChangeEvent } from 'react'
+import dynamic from 'next/dynamic'
+import type { FeatureCollection, Feature } from 'geojson'
+
+
+export default function DistrictResultsPage() {
+  
+
+  return (
+    <div className="page3 flex flex-col items-start space-y-4 w-full">
+      Hello World
+    </div>
+  )
+}
